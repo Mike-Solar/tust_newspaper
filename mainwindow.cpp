36037d0ee26d4e77087c13f94d13aa78b2a37ee5
Mike Solar <iam@mikesolar.cn>
2025-06-01 14:53:56 +0800

    new:一部分信号和槽

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,14 +11,19 @@
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    titleLabel = new QLabel("本版标题",this);
     titleEdit=new QLineEdit(this);
+    dateLabel = new QLabel("日期和版数",this);
     dateEdit=new QLineEdit(this);
+    editorsLabel = new QLabel("编辑",this);
     editorsEdit=new QLineEdit(this);
     isFirstPageCheckBox=new QCheckBox(this);
-    addArticleButton=new QPushButton(this);
-    saveButton=new QPushButton(this);
-    exportButton=new QPushButton(this);
-    editHeadNewsButton=new QPushButton(this);
+    isFirstPageCheckBox->setText("是否为第一版");
+    isFirstPageCheckBox->setChecked(false);
+    addArticleButton=new QPushButton("添加文章",this);
+    saveButton=new QPushButton("保存",this);
+    exportButton=new QPushButton("导出PDF",this);
+    editHeadNewsButton=new QPushButton("编辑头条",this);
     headDialog=new HeadDialog(this);
     headDialog->close();
     // 布局
@@ -27,25 +32,28 @@ MainWindow::MainWindow(QWidget *parent) :
     // 是不是第一版
     headLayout.addWidget(isFirstPageCheckBox);
     headLayout.addWidget(editHeadNewsButton);
-    mainLayout.addLayout(&headLayout);
+    mainLayout.addLayout(&headLayout, 0, 0, 1, 2);
 
-    // 是不是第一版都要有
-    mainLayout.addWidget(titleEdit);
-    mainLayout.addWidget(dateEdit);
-    mainLayout.addWidget(editorsEdit);
+    // 不管是不是第一版都要有
+    mainLayout.addWidget(titleLabel, 1, 0, 1, 1);
+    mainLayout.addWidget(titleEdit, 1, 1, 1, 1);
+    mainLayout.addWidget(dateLabel, 2, 0, 1, 1);
+    mainLayout.addWidget(dateEdit, 2, 1, 1, 1);
+    mainLayout.addWidget(editorsLabel, 3, 0, 1, 1);
+    mainLayout.addWidget(editorsEdit, 3, 1, 1, 1);
 
     // 按钮
     articleButtonLayout.addWidget(addArticleButton);
     articleButtonLayout.addWidget(saveButton);
     articleButtonLayout.addWidget(exportButton);
-    mainLayout.addLayout(&articleButtonLayout);
+    mainLayout.addLayout(&articleButtonLayout, 4, 0, 1, 2);
 
     editHeadNewsButton->setDisabled(true);
 
     // 初始化信号和槽
     connect(isFirstPageCheckBox, &QCheckBox::stateChanged, this, &MainWindow::isFirstPageChecked);
     connect(editHeadNewsButton, &QPushButton::clicked, this, &MainWindow::editHeadNewsButtonClicked);
-
+    connect(addArticleButton, &QPushButton::clicked, this, &MainWindow::newArticle);
 }
 
 MainWindow::~MainWindow() {
@@ -64,4 +72,12 @@ void MainWindow::isFirstPageChecked(int checked) {
 void MainWindow::editHeadNewsButtonClicked() {
     headDialog->show();
     headDialog->open();
+}
+
+void MainWindow::newArticle() {
+    articleInputs.append(ArticleInput(std::forward<QVBoxLayout>(articleListLayout), this));
+}
+
+void MainWindow::deleteArticle() {
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,27 +15,67 @@
 #include <QCheckBox>
 #include "Article.h"
 #include "HeadDialog.h"
+#include <QGridLayout>
+#include <QLabel>
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+
+namespace Ui {
+    class MainWindow;
+}
+
 QT_END_NAMESPACE
 
-class ArticleInput: public QObject {
+class ArticleInput : public QObject {
 private:
     Q_OBJECT
+    QLabel *titleLabel;
     QLineEdit *title;
+    QLabel *fromLabel;
     QLineEdit *from;
+    QLabel *contentLabel;
     QTextEdit *content;
     QPushButton *addPictureButton;
+    QList<QLabel > *pictureLabels;
     QList<QPushButton> *deletePictureButtons;
     QPushButton *deleteArticleButton;
+    QGridLayout *mainLayout;
+    QGridLayout *pictureLayout;
+
+    QWidget *parent=nullptr;
 public:
-    ArticleInput(QWidget* parent=nullptr) {
-        title=new QLineEdit(parent);
-        from=new QLineEdit(parent);
-        content=new QTextEdit(parent);
-        addPictureButton=new QPushButton(parent);
-        deleteArticleButton=new QPushButton(parent);
-        deletePictureButtons=new QList<QPushButton>;
+    ArticleInput(QVBoxLayout &&layout,QWidget *parent = nullptr) {
+        this->parent = parent;
+
+        title = new QLineEdit(parent);
+        titleLabel = new QLabel(parent);
+        from = new QLineEdit(parent);
+        fromLabel = new QLabel(parent);
+        content = new QTextEdit(parent);
+        contentLabel = new QLabel(parent);
+        addPictureButton = new QPushButton(parent);
+        deleteArticleButton = new QPushButton(parent);
+        deletePictureButtons = new QList<QPushButton>;
+        mainLayout = new QGridLayout(parent);
+        mainLayout->addWidget(titleLabel, 0, 0, 1, 1);
+        mainLayout->addWidget(title, 0, 1, 1, 1);
+        mainLayout->addWidget(fromLabel, 1, 0, 1, 1);
+        mainLayout->addWidget(from, 1, 1, 1, 1);
+        mainLayout->addWidget(contentLabel, 2, 0, 1, 2);
+        mainLayout->addWidget(content, 3, 0, 5, 2);
+        mainLayout->addWidget(addPictureButton, 4, 0, 1, 2);
+        pictureLayout = new QGridLayout(parent);
+        pictureLayout->setColumnStretch(0, 6);
+        pictureLayout->setColumnStretch(1, 1);
+        mainLayout->addLayout(pictureLayout, 5, 0, 5, 2);
+
+        layout.addLayout(mainLayout);
+    }
+public slots:
+    void addPicture(QString path) {
+        pictureLabels->append(QLabel(path,parent));
+        deletePictureButtons->append(QPushButton(parent));
+        pictureLayout->addWidget(&pictureLabels->last(), 0, 0, 1, 1);
+        pictureLayout->addWidget(&deletePictureButtons->last(), 0, 1, 1, 1);
     }
     ~ArticleInput() {
         delete deletePictureButtons;
@@ -44,19 +84,27 @@ public:
 
 
 class MainWindow : public QMainWindow {
-Q_OBJECT
+    Q_OBJECT
 signals:
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow() override;
+
 public slots:
     void isFirstPageChecked(int checked);
+
     void editHeadNewsButtonClicked();
+
     void newArticle();
+
     void deleteArticle();
+
     void addPicture();
+
     void deletePicture();
+
 private:
     Ui::MainWindow *ui;
     // 数据
@@ -67,21 +115,25 @@ private:
     bool isFirstPage;
     Article head; // 头版
     // 控件
+    QLabel *titleLabel;
     QLineEdit *titleEdit;
+    QLabel *dateLabel;
     QLineEdit *dateEdit;
+    QLabel *editorsLabel;
     QLineEdit *editorsEdit;
     QCheckBox *isFirstPageCheckBox;
     QPushButton *addArticleButton;
     QPushButton *saveButton;
-    QPushButton* exportButton;
+    QPushButton *exportButton;
     QPushButton *editHeadNewsButton;
     QList<ArticleInput> articleInputs;
-    QVBoxLayout mainLayout;
+    QGridLayout mainLayout;
+    QVBoxLayout articleListLayout;
+    QGridLayout articleLayout;
     QHBoxLayout articleButtonLayout;
     QHBoxLayout headLayout;
 
     HeadDialog *headDialog;
-
 };
 
 
